18327b65ad2bc5599c1c7ee614674a42ccf2de6d
Simone Bacchio <s.bacchio@gmail.com>
2020-08-26 12:52:55 +0300

    Adding dask dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from lyncs_setuptools import setup, CMakeExtension
 
+requirements = [
+    "mpi4py",
+    "lyncs-cppyy",
+    "dask",
+    "dask[array]",
+    "dask[distributed]",
+    "dask-mpi",
+    "sh",
+]
+
+
 setup(
     "lyncs_mpi",
     exclude=["*.config"],
     ext_modules=[CMakeExtension("lyncs_mpi.lib", ".")],
     data_files=[(".", ["config.py.in"])],
-    install_requires=["mpi4py", "lyncs-cppyy", "dask", "dask-mpi", "sh"],
+    install_requires=requirements,
     extras_require={"test": ["pytest", "pytest-cov",]},
     keywords=["Lyncs", "MPI", "mpi4py", "dask-mpi",],
 )
