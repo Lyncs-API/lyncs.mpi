abd7a9b20eee20cc77fd96a54402d708e558bbee
Simone Bacchio <s.bacchio@gmail.com>
2020-09-17 14:09:48 +0300

    Documenting

--- lyncs_mpi/abc.py
+++ lyncs_mpi/abc.py
@@ -5,15 +5,19 @@ Some ABC to be used for typing parallel classes
 from abc import ABC
 from numpy import ndarray
 from dask.array import Array as daskArray
-from distributed import as_completed
+from .cartesian import Cartesian
 
 
 class Result(ABC):
+    "Returns a tuple with the result of the remote calls"
+
     def __new__(cls, ftrs):
         return tuple(ftr.result() for ftr in ftrs)
 
 
 class Global(Result):
+    "Returns a single value supposed to be the same result for all the remote calls"
+
     def __new__(cls, ftrs):
         res = super().__new__(cls, ftrs)
         ret = res[0]
@@ -23,7 +27,10 @@ class Global(Result):
 
 
 class Array(Result):
+    "Returns a dask array supposing the ftrs output to be numpy-like arrays"
+
     def __new__(cls, ftrs):
+        assert isinstance(ftrs, Cartesian)
         return ftrs.comm.array(ftrs)
 
 
